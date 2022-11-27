93097331d066bcad241307c5120cd5b0c06d5818
rohit1246 <rooohit456@gmail.com>
2022-11-27 21:22:03 +0530

    third commit

--- customuser/settings.py
+++ customuser/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +138,5 @@ MEDIA_ROOT = BASE_DIR / 'static/images/user_profile_pics'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
